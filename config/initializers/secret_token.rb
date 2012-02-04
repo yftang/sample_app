b629a6d0a1eada927443eba13c34d8ac0da7f0ed
Eragon Tang <tangyifei.tang@gmail.com>
2012-02-04 13:41:22 +0800

    Created application direcotory, modified Gemfile and database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '36268a264406c2533eeefbfafdc4b50c5c1a953fcaf92cdbb2d3474e27f0e48e7ef489194877555e53919696b820adb72404d1d85992b67b8654e8069f0cb364'
