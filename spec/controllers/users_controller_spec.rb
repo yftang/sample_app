6678a7b1eb9f5cbcd1ceb999088920ee7d783b62
Eragon Tang <tangyifei.tang@gmail.com>
2012-02-12 22:02:23 +0800

    Added a signup page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -47,12 +47,11 @@ describe UsersController do
       get :new
       response.should be_success
     end
-  end
 
     it "should have the right title" do
       get :new
       response.should have_selector("title",
                                     :content => "Sign up")
     end
-
+  end
 end
