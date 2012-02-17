e107731e9c2c5a5776098fe28e8a439c305c494e
Eragon Tang <tangyifei.tang@gmail.com>
2012-02-17 09:54:53 +0800

    Friendly forwarding and correct user requirement

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,10 @@ module SessionsHelper
     @current_user = nil
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def current_user=(user)
     @current_user = user
   end
@@ -22,7 +26,21 @@ module SessionsHelper
   end
     
   def deny_access
-   redirect_to signin_path, :notice => "Please sign in to access this page." 
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page." 
+  end
+  
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+  
+  def redirect_back_or(default)
+    redirect_to session[:return_to] || default
+    clear_return_to
+  end
+  
+  def clear_return_to
+    session[:return_to] = nil
   end
   
   private
