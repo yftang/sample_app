8b83584fe7b85b2a357a335f5b46f13131c9ce38
Eragon Tang <tangyifei.tang@gmail.com>
2012-02-29 13:01:12 +0800

    Made a working feed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
+  
   attr_accessible :content
   
   belongs_to :user
@@ -8,4 +9,16 @@ class Micropost < ActiveRecord::Base
   validates :user_id, :presence => true
   
   default_scope :order => 'microposts.created_at DESC'
+  
+  scope :from_users_followed_by, lambda { |user| followed_by(user) }
+  
+  private
+    def self.followed_by(user)
+      # followed_ids = user.following.map(&:id).join(", ").inspect
+      followed_ids = %(SELECT followed_id FROM relationships
+                       WHERE follower_id = :user_id)
+      where("user_id IN (#{followed_ids}) OR user_id = :user_id",
+            :user_id => user.id)
+    end
+    
 end
